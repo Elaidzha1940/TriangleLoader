05851b58836d824e61e0702069e01927eafdf9ce
Elaidzha1940 <eli080293@gmail.com>
2023-12-20 15:18:06 +0300

    TriangleLoader

---
+++ TriangleLoader/TriangleLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriangleLoaderApp.swift
+//  TriangleLoader
+//
+//  Created by Elaidzha Shchukin on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TriangleLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
