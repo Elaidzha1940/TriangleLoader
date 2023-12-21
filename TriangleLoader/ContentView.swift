d86eab5ebdefb9449a44696d3cde08f7f189bcf1
Elaidzha1940 <eli080293@gmail.com>
2023-12-21 15:14:57 +0300

    Update ContentView

--- TriangleLoader/ContentView.swift
+++ TriangleLoader/ContentView.swift
@@ -53,8 +53,71 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-           
+            Color("Bg")
+                .ignoresSafeArea()
+            
+            ZStack {
+                TriangleShape()
+                    .trim(from: strokeStart, to: strokeEnd)
+                    .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round, miterLimit: 8))
+                    .foregroundColor(.white.opacity(0.5))
+                Circle()
+                    .offset(circleOffset)
+                    .foregroundColor(.mint)
+                    .frame(width: 18, height: 18)
+            }
+            .frame(width: 100, height: 100)
         }
+        .onAppear {
+            getStroke(state: .begin)
+            setCircleOffset(state: .begin)
+            animate()
+            Timer.scheduledTimer(withTimeInterval: animationDuration * 4.5, repeats: true) {_ in 
+                animate()
+            }
+        }
+    }
+    
+    func animate() {
+        Timer.scheduledTimer(withTimeInterval: animationDuration / 2, repeats: false) {_ in
+            withAnimation(.easeInOut(duration: animationDuration)) {
+                getStroke(state: .phaseOne)
+            }
+            
+            withAnimation(.spring(response: animationDuration * 2, dampingFraction: 0.85)) {
+                setCircleOffset(state: .phaseOne)
+            }
+        }
+        Timer.scheduledTimer(withTimeInterval: animationDuration * 2, repeats: false) {_ in
+            withAnimation(.easeInOut(duration: animationDuration)) {
+                getStroke(state: .phaseTwo)
+            }
+            withAnimation(.spring(response: animationDuration * 2, dampingFraction: 0.85)) {
+                setCircleOffset(state: .phaseTwo)
+            }
+        }
+        Timer.scheduledTimer(withTimeInterval: animationDuration * 3.5, repeats: false) {_ in
+            setCircleOffset(state: .stop)
+            
+            withAnimation(.easeInOut(duration: animationDuration)) {
+                getStroke(state: .begin)
+            }
+            withAnimation(.spring(response: animationDuration)) {
+                getStroke(state: .begin)
+            }
+            withAnimation(.spring(response: animationDuration * 2, dampingFraction: 0.85)) {
+                setCircleOffset(state: .begin)
+            }
+        }
+    }
+    
+    func getStroke(state: TriangleState) {
+        (self.strokeStart, self.strokeEnd) = state.getStrokes()
+    }
+    
+    func setCircleOffset(state: TriangleState) {
+        let offset = state.getCircleOffset()
+        self.circleOffset = CGSize(width: offset.0, height: offset.1)
     }
 }
 
